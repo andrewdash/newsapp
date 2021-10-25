f60b9b1078d71468b16a92e7ac95d58e986b38a4
andrewdash <andrewdashes@gmail.com>
2021-10-24 20:08:36 -0700

    Update api.py

--- api.py
+++ api.py
@@ -18,8 +18,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = ["http://localhost:3000", "https://andrewdash.github.io"]
-origins = ["*"]
+origins = ["http://localhost:3000", "https://andrewdash.github.io", "http://andrewdash.github.io"]
 # what is a middleware? 
 # software that acts as a bridge between an operating system or database and applications, especially on a network.
 app.add_middleware(
