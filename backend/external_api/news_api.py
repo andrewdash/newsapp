333a646c7e49c8a0ad18152227df74d47fe5a3fa
andrewdash <andrewdashes@gmail.com>
2021-10-29 13:38:25 -0700

    implement additional new sources

--- backend/external_api/news_api.py
+++ backend/external_api/news_api.py
@@ -5,6 +5,21 @@ import dateutil.parser as dp
 from backend.config import NEWS_API_KEY
 from backend.external_api.models import NewsBase
 
+# A comma-seperated string of identifiers for the news sources or blogs you want headlines from
+# https://newsapi.org/docs/endpoints/top-headlines
+SOURCES = """
+    'bbc-news', 'cnn', 'bloomberg', 'the-wall-street-journal', 'financial-times',
+    'axios', 'bbc-sport', 'cbc-news', 'hacker-news'
+"""
+def get_sources(sources):
+    sources = sources.replace(" ", "")
+    sources = sources.replace("\'", "")
+    sources = sources.replace("\"", "")
+    sources = sources.strip()
+    return sources
+
+CHOSEN_NEWS_SOURCES = get_sources(SOURCES)
+
 def newsapi_response_parser(results) -> List[NewsBase]:
     """
     :param results: JSON Object
@@ -13,7 +28,7 @@ def newsapi_response_parser(results) -> List[NewsBase]:
                 {
                     "title": "title of the news",
                     "link": "original link of the news source",
-                    "source":"your-api-name"
+                    "api_source":"your-api-name"
                 },
             ...
             ]
@@ -26,16 +41,19 @@ def newsapi_response_parser(results) -> List[NewsBase]:
             title=child["title"],
             link=child["url"],
             created_utc=created_utc,
-            source="newsapi",
+            news_source=child["source"]["name"],
+            api_source="newsapi",
         )
         response.append(news_base)
     # if nothing was added to the response list, append an empty NewsBase
     if not response:
+        print("no response found for newsapi...")
         empty_news_base = NewsBase(
             title="No items found!",
             link="",
             created_utc=datetime.today().timestamp(),
-            source="newsapi"
+            news_source="No source found",
+            api_source="newsapi"
         )
         response.append(empty_news_base)
     # sort the response by created_utc
@@ -46,12 +64,12 @@ def newsapi_response_parser(results) -> List[NewsBase]:
 # 'limit' and 'query' are provided when you call 
 # NEWS_API_MAPPING['listing_url'] or NEWS_API_MAPPING['search_url']
 NEWS_API_MAPPING = {
-    "api_name": "newsapi",
+    "api_source": "newsapi",
     "parser": newsapi_response_parser,
-    "listing_url": 'http://newsapi.org/v2/top-headlines?language=en&category=general&pageSize={limit}&page=1&' +
-                   'apiKey={api_key}'.format(api_key=NEWS_API_KEY),
+    "listing_url": 'http://newsapi.org/v2/top-headlines?language=en&pageSize={limit}&page=1&' +
+                   'sources={sources}&apiKey={api_key}'.format(api_key=NEWS_API_KEY, sources=CHOSEN_NEWS_SOURCES),
     "search_url": 'http://newsapi.org/v2/everything?language=en&q={query}&pageSize={limit}&page=1&' +
-                  'apiKey={api_key}'.format(api_key=NEWS_API_KEY),
+                  'sources={sources}&apiKey={api_key}'.format(api_key=NEWS_API_KEY, sources=CHOSEN_NEWS_SOURCES),
     "headers": "",
 }
 
